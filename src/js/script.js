48324f0506e3c58207d374ad9a830fcc803b7bf6
stan2590 <evan.gay@edu.univ-fcomte.fr>
2024-11-15 08:42:49 +0100

    ouverture/fermeture menu.

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,6 @@ toggle.addEventListener("click", () => {
     nav.setAttribute("aria-hidden", nav.getAttribute("aria-hidden") === "true" ? "false" : "true");
     toggle.setAttribute("aria-expanded", toggle.getAttribute("aria-expanded") === "false" ? "true" : "false");
   });
+  const isOpen = toggle.ariaExpanded === "true";
+const isClosed = !isOpen;
+console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
